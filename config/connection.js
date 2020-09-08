124a0386f7e9d32c6750474056bab9d58e5b8257
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-09-07 22:51:28 -0400

    changes to connect to heroku hosted mySQL DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    "password": "Kambiz83",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect((err) => {
     if (err) {
